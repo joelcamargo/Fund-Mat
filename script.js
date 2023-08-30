1608e30e0b22b39acf6f026402d8b10fdcb6403d
joelcamargo <37198694+joelcamargo@users.noreply.github.com>
2023-08-29 22:48:48 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,28 +1,31 @@
+
 function éPrimo(num) {
-  if (num <= 1) return false;
-  if (num <= 3) return true;
-  if (num % 2 === 0 || num % 3 === 0) return false;
-  for (let i = 5; i * i <= num; i += 6) {
-    if (num % i === 0 || num % (i + 2) === 0) return false;
+  if (num <= 3) {
+    return true;
+  }
+  if (num % 2 === 0 || num % 3 === 0) {
+    return false;
   }
   return true;
+   
 }
 
+
 document
-  .getElementById("sortButton")
+  .getElementById("sorteador")
   .addEventListener("click", function () {
-    const resultadoDiv = document.getElementById("result");
+    const resultadoDiv = document.getElementById("resultado");
     resultadoDiv.innerHTML = "";
 
-    const números = [];
+    const numeros = [];
     for (let i = 0; i < 10; i++) {
-      const númeroAleatório = Math.floor(Math.random() * 20) + 1;
-      números.push(númeroAleatório);
+      const numeroAleatório = Math.floor(Math.random() * 20) + 1;
+      numeros.push(numeroAleatório);
     }
 
-    números.forEach((num) => {
+    numeros.forEach((num) => {
       const parágrafo = document.createElement("p");
-      parágrafo.textContent = `Número: ${num} - ${
+      parágrafo.textContent = `numero: ${num} - ${
         num % 2 === 0 ? "Par" : "Ímpar"
       } - ${éPrimo(num) ? "Primo" : "Não Primo"}`;
       resultadoDiv.appendChild(parágrafo);
